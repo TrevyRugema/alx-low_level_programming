67cbeebf21929d802eb89cafaa5acb2d2c67a388
TrevyRugema <trevyrugema@gmail.com>
2023-02-18 06:13:18 -0800

    resoved error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Entry Point
  * Description: A Program that prints a line with puts function
  * Return : 0 (Sucess)
- */
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
