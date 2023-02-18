4c679ba3c166672d5c87b36c4278180ec59c2658
TrevyRugema <trevyrugema@gmail.com>
2023-02-18 07:01:58 -0800

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,17 @@
+Sure, here's the same program with the Betty style:
+
+c
+Copy code
 #include <stdio.h>
+
 /**
- * main - Entry Point
- * Description: 'A Program that use printf function'
- * Retrun : 0(Sucess)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return (0);
 }
