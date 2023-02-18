1baab2f8de228482afa499df841cb07a1c7775d9
TrevyRugema <trevyrugema@gmail.com>
2023-02-18 05:56:43 -0800

    Write a C program that prints exactl Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - Program that prints a line with puts function
+ * Return : 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle,");
+return (0);
+}
