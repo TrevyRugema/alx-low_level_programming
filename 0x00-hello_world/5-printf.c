7bef22649baa26fc3d72ad45981ec33f49235c7f
TrevyRugema <trevyrugema@gmail.com>
2023-02-18 06:43:59 -0800

    with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: 'A Program that use printf function'
+ * Retrun : 0(Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
