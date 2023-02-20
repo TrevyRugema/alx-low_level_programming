5c0e1680d2617e524101c644d49b35c164614f0e
TrevyRugema <trevyrugema@gmail.com>
2023-02-20 15:29:22 -0800

    a C program that prints exactly and that piece of art is useful followed by a new line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <unistd.h>
-int main(void) {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    const size_t len = sizeof(msg) - 1; // subtract 1 to exclude null terminator
 
-    ssize_t nwritten = write(STDERR_FILENO, msg, len);
-    if (nwritten != len) {
-        return 1;
-    }
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const size_t len = sizeof(msg) - 1; /* Subtract 1 to exclude null terminator */
+	ssize_t nwritten;
 
-    return 0;
+	nwritten = write(STDERR_FILENO, msg, len);
+	if (nwritten != (ssize_t)len)
+	{
+		return (1);
+	}
+
+	return (0);
 }
