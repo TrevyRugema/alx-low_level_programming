07d9ddcd0a28791086e8ab7ebb62b190a57dfd3f
TrevyRugema <trevyrugema@gmail.com>
2023-02-20 17:23:02 -0800

    Prints single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single digit numbers of base 10 starting from 0
+ * without using any variables of type char and only using putchar twice
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    putchar('0' + 0);
+    putchar('0' + 1);
+    putchar('0' + 2);
+    putchar('0' + 3);
+    putchar('0' + 4);
+    putchar('0' + 5);
+    putchar('0' + 6);
+    putchar('0' + 7);
+    putchar('0' + 8);
+    putchar('0' + 9);
+
+    putchar('\n');
+
+    return (0);
+}
