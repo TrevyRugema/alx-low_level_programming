7d4589874b4907c9e5ed1bdcc4b7cd6b01b2c473
TrevyRugema <trevyrugema@gmail.com>
2023-02-20 17:27:48 -0800

    Prints single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,16 +10,13 @@
  */
 int main(void)
 {
-    putchar('0' + 0);
-    putchar('0' + 1);
-    putchar('0' + 2);
-    putchar('0' + 3);
-    putchar('0' + 4);
-    putchar('0' + 5);
-    putchar('0' + 6);
-    putchar('0' + 7);
-    putchar('0' + 8);
-    putchar('0' + 9);
+    int num = 0;
+
+    while (num <= 9)
+    {
+        putchar(num + '0');
+        num++;
+    }
 
     putchar('\n');
 
