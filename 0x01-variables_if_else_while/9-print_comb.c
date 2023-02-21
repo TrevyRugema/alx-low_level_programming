9b6b02e86187ff577265c557c9de561ccbfa1b88
TrevyRugema <trevyrugema@gmail.com>
2023-02-20 17:41:42 -0800

    program in C that prints all possible combinations of single-digit numbers using only the putchar function four times maximum

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,24 +10,19 @@
 int main(void)
 {
     int i, j;
-
     for (i = 0; i < 9; i++)
     {
         for (j = i + 1; j <= 9; j++)
         {
             putchar(i + '0');
-            putchar(',');
-            putchar(' ');
             putchar(j + '0');
-            if (i != 8 || j != 9)
+            if (i < 8)
             {
                 putchar(',');
                 putchar(' ');
             }
         }
     }
-
     putchar('\n');
-
     return (0);
 }
